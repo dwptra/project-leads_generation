6595aa07bb6c59eeff169b3f2a999e0e17c700cd
12108541muhamad <muhamaddwiputranovriansyah@smkwikrama.sch.id>
2023-04-10 15:46:38 +0700

    Membuat Leads (Controller, Migrations, Models, Routes)

---
+++ app/Http/Controllers/LeadsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Leads;
+use Illuminate\Http\Request;
+
+class LeadsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Leads  $leads
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Leads $leads)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Leads  $leads
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Leads $leads)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Leads  $leads
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Leads $leads)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Leads  $leads
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Leads $leads)
+    {
+        //
+    }
+}
