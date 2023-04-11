f9ede49a93e3efe63b53560c6d0050e0a93f7073
Carmilla <danielprawirasastra@smkwikrama.sch.id>
2023-04-11 09:46:39 +0700

    Tambah seeders owner

---
+++ database/seeders/OwnerSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Owner;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class OwnerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        Owner::create([
+                'name' => 'Dwi',
+                'password' => bcrypt('123'),   
+            ]);
+    }
+}
