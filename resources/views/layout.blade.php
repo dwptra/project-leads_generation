7bba0b3a773714b6876b50ca9951228a71dbde14
12108541muhamad <muhamaddwiputranovriansyah@smkwikrama.sch.id>
2023-04-11 11:33:16 +0700

    Tambah link bootstrap icon

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,6 +9,7 @@
     <meta name="author" content="" />
     <title>Leads Generation</title>
     <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <link href="css/styles.css" rel="stylesheet" />
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
 </head>
