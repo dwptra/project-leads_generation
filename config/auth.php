a4238f902da145b615ec445b4fdb7bbee2fa2ffd
12108541muhamad <muhamaddwiputranovriansyah@smkwikrama.sch.id>
2023-04-11 10:42:41 +0700

    Ubah auth.php dari Users ke Owners

--- config/auth.php
+++ config/auth.php
@@ -38,7 +38,7 @@ return [
     'guards' => [
         'web' => [
             'driver' => 'session',
-            'provider' => 'users',
+            'provider' => 'owners',
         ],
     ],
 
@@ -60,9 +60,9 @@ return [
     */
 
     'providers' => [
-        'users' => [
+        'owners' => [
             'driver' => 'eloquent',
-            'model' => App\Models\User::class,
+            'model' => App\Models\Owner::class,
         ],
 
         // 'users' => [
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -51,7 +51,7 @@
                 <div class="sb-sidenav-menu">
                     <div class="nav">
                         <div class="sb-sidenav-menu-heading">Core</div>
-                        <a class="nav-link" href="index.html">
+                        <a class="nav-link" href="/dashboard">
                             <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                             Dashboard
                         </a>
@@ -121,7 +121,7 @@
                 </div>
                 <div class="sb-sidenav-footer">
                     <div class="small">Logged in as:</div>
-                    Start Bootstrap
+                    {{ auth()->user()->name }}
                 </div>
             </nav>
         </div>
