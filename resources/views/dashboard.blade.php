dc4f4fe7597ebdbc6be1780074172f3ccfc621da
12108541muhamad <muhamaddwiputranovriansyah@smkwikrama.sch.id>
2023-04-14 15:18:35 +0700

    Perbaiki dashboard page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,15 +3,13 @@
 <main>
     <div class="container-fluid px-4">
         <h1 class="mt-4">Dashboard</h1>
+        <hr>
         @if (Session::get('notAllowed'))
         <div class="alert alert-danger w-100" role="alert">
             {{ Session::get('notAllowed')}}
             <button type="button" class="btn-close float-end" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
         @endif
-        <ol class="breadcrumb mb-4">
-            <li class="breadcrumb-item active">Hi, {{ auth()->user()->name }}</li>
-        </ol>
 
         @foreach($owners as $owner)
         <div class="row">
