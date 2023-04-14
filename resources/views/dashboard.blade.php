feeae29b1958c3b2a72e03dc053444a989f85bf3
Carmilla <danielprawirasastra@smkwikrama.sch.id>
2023-04-14 14:34:51 +0700

    Tambah status per owner di dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,23 +13,49 @@
             <li class="breadcrumb-item active">Hi, {{ auth()->user()->name }}</li>
         </ol>
 
+        @foreach($owners as $owner)
         <div class="row">
+            <h3>{{ $owner->name }}</h3>
+            @php
+                $mqlCount = App\Models\Leads::where('owner_id', $owner->id)->where('status', 'MQL')->count();
+                $pqlCount = App\Models\Leads::where('owner_id', $owner->id)->where('status', 'PQL')->count();
+                $sqlCount = App\Models\Leads::where('owner_id', $owner->id)->where('status', 'SQL')->count();
+                $srqlCount = App\Models\Leads::where('owner_id', $owner->id)->where('status', 'SrQL')->count();
+            @endphp
             <div class="col-xl-3 col-md-6">
                 <div class="card bg-primary text-white mb-4">
-                    <div class="card-body">Users Account</div>
+                    <div class="card-body">MQL</div>
                     <div class="card-footer d-flex align-items-center justify-content-between">
-                        <p class="small text-white">Total : {{ $userCount }}</p>
+                        <p class="small text-white">Total : {{ $mqlCount }}</p>
                     </div>
                 </div>
             </div>
             <div class="col-xl-3 col-md-6">
                 <div class="card bg-success text-white mb-4">
-                    <div class="card-body">Leads</div>
+                    <div class="card-body">PQL</div>
                     <div class="card-footer d-flex align-items-center justify-content-between">
-                        <p class="small text-white">Total : {{ $leadsCount }}</p>
+                        <p class="small text-white">Total : {{ $pqlCount }}</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-md-6">
+                <div class="card bg-warning text-white mb-4">
+                    <div class="card-body">SQL</div>
+                    <div class="card-footer d-flex align-items-center justify-content-between">
+                        <p class="small text-white">Total : {{ $sqlCount }}</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-md-6">
+                <div class="card bg-danger text-white mb-4">
+                    <div class="card-body">SrQL</div>
+                    <div class="card-footer d-flex align-items-center justify-content-between">
+                        <p class="small text-white">Total : {{ $srqlCount }}</p>
                     </div>
                 </div>
             </div>
         </div>
+        @endforeach
+    </div>
 </main>
 @endsection
