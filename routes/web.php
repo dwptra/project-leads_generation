c84ae6845ef89eacab0a63079bed88abff17625d
Carmilla <danielprawirasastra@smkwikrama.sch.id>
2023-05-09 09:13:43 +0700

    rapihkan url agar lebih jelas tujuannya untuk apa

--- routes/web.php
+++ routes/web.php
@@ -29,35 +29,34 @@ use App\Http\Middleware\cekRole;
 Route::middleware(['cekRole', 'isLogin'])->group(function () {
     // User
     Route::prefix('user')->group(function () {
-        Route::get('/create', [UserController::class, 'userCreate'])->name('user.create');
-        Route::post('/create', [UserController::class, 'userPost'])->name('user.post');
-        Route::get('/edit/{id}', [UserController::class, 'userEdit'])->name('user.edit');
-        Route::patch('/update/{id}', [UserController::class, 'userUpdate'])->name('user.update');    
-        Route::delete('/delete/{id}', [UserController::class, 'userDelete'])->name('user.delete');
+        Route::get('/formcreateuser', [UserController::class, 'userCreate'])->name('user.create');
+        Route::post('/createuser', [UserController::class, 'userPost'])->name('user.post');
+        Route::get('/formedituser/{id}', [UserController::class, 'userEdit'])->name('user.edit');
+        Route::patch('/updateuser/{id}', [UserController::class, 'userUpdate'])->name('user.update');    
+        Route::delete('/deleteuser/{id}', [UserController::class, 'userDelete'])->name('user.delete');
     });
 
     // Owner
     Route::prefix('owner')->group(function () {
-        Route::post('/create', [OwnerController::class, 'ownerPost'])->name('owner.post');
-        Route::patch('/update/{id}', [OwnerController::class, 'ownerUpdate'])->name('owner.update');  
-        Route::delete('/delete/{id}', [OwnerController::class, 'ownerDelete'])->name('owner.delete');
+        Route::post('/formreateowner', [OwnerController::class, 'ownerPost'])->name('owner.post');
+        Route::patch('/formupdateowner/{id}', [OwnerController::class, 'ownerUpdate'])->name('owner.update');  
+        Route::delete('/formdeleteowner/{id}', [OwnerController::class, 'ownerDelete'])->name('owner.delete');
     });
 
     // Leads
     Route::prefix('leads')->group(function () {
-        Route::get('/create', [LeadsController::class, 'leadsCreate'])->name('leads.create');
-        Route::post('/create', [LeadsController::class, 'leadsPost'])->name('leads.post');
-        Route::get('/edit/{id}', [LeadsController::class, 'leadsEdit'])->name('leads.edit');
-        Route::patch('/update/{id}', [LeadsController::class, 'leadsUpdate'])->name('leads.update');
-        Route::delete('/delete/{id}', [LeadsController::class, 'leadsDelete'])->name('leads.delete');
-        Route::get('/export', [LeadsController::class, 'exportLeadsToExcel'])->name('exportLeadsToExcel');
+        Route::get('/formcreateleads', [LeadsController::class, 'leadsCreate'])->name('leads.create');
+        Route::post('/createleads', [LeadsController::class, 'leadsPost'])->name('leads.post');
+        Route::get('/formeditleads/{id}', [LeadsController::class, 'leadsEdit'])->name('leads.edit');
+        Route::patch('/updateleads/{id}', [LeadsController::class, 'leadsUpdate'])->name('leads.update');
+        Route::delete('/deleteleads/{id}', [LeadsController::class, 'leadsDelete'])->name('leads.delete');
+        Route::get('/exportleads', [LeadsController::class, 'exportLeadsToExcel'])->name('exportLeadsToExcel');
     });
     
     // Histories
-    Route::prefix('leads_histories')->group(function () {
+    Route::prefix('leadshistories')->group(function () {
         Route::get('/', [LeadsController::class, 'leadsHistories'])->name('leads.histories');
-        Route::get('/leads/{id}/histories', [LeadsController::class, 'showHistories'])->name('show.histories');
-        Route::delete('/delete/{id}', [LeadsController::class, 'historiesDelete'])->name('histories.delete');
+        Route::delete('/deletehistories/{id}', [LeadsController::class, 'historiesDelete'])->name('histories.delete');
     });
 });
 
@@ -67,8 +66,8 @@ Route::middleware('isLogin')->group(function () {
     Route::get('/user', [UserController::class, 'user'])->name('user.index');
     Route::get('/owner', [OwnerController::class, 'owner'])->name('owner');
     Route::get('/leads', [LeadsController::class, 'leads'])->name('leads');
-    Route::get('/leads_report', [LeadsController::class, 'leadsReport'])->name('leads.report');
-    Route::get('/report', [LeadsController::class, 'generateReport'])->name('generate.report');
+    Route::get('/leadsreport', [LeadsController::class, 'leadsReport'])->name('leads.report');
+    Route::get('/generatereport', [LeadsController::class, 'generateReport'])->name('generate.report');
     Route::get('/dashboard', [Controller::class, 'dashboard'])->name('dashboard');
 });
 
